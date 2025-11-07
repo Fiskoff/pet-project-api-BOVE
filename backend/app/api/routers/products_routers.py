e5c57063630c775110da556928fd01c5b001d503
Fiskoff <fiskoff2015@yandex.ru>
2025-11-07 12:56:52 +0700

    Created all endpoints

--- backend/app/api/routers/products_routers.py
+++ backend/app/api/routers/products_routers.py
@@ -2,32 +2,40 @@ import logging
 
 from fastapi import APIRouter
 
+from app.services import ProductCategoryService, ProductService
+from app.api.schemas import GetCategoriesResponse, GetCategoryByIdResponse, GetProductsResponse, GetProductByIdResponse, GetProductVariantsResponse
+
 
 logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/products", tags=["Products"])
 
 
-@router.get("/categories", summary="Получить все категории товаров")
+@router.get("/categories", summary="Получить все категории товаров", response_model=GetCategoriesResponse)
 async def get_categories():
-    pass
+    categories = await ProductCategoryService.get_all()
+    return GetCategoriesResponse(categories=categories)
 
 
-@router.get("/categories/{category_id}", summary="Получить конкретную категорию товара и её продукты")
+@router.get("/categories/{category_id}", summary="Получить конкретную категорию товара и её продукты", response_model=GetCategoryByIdResponse)
 async def get_category(category_id: int):
-    pass
+    category_with_products = await ProductCategoryService.get_by_id(category_id)
+    return GetCategoryByIdResponse(category=category_with_products)
 
 
-@router.get("", summary="Получить все продукты")
+@router.get("", summary="Получить все продукты", response_model=GetProductsResponse)
 async def get_products():
-    pass
+    products = await ProductService.get_all()
+    return GetProductsResponse(products=products)
 
 
-@router.get("/{product_id}", summary="Получить конкретный продукт и его варианты")
+@router.get("/{product_id}", summary="Получить конкретный продукт и его варианты", response_model=GetProductByIdResponse)
 async def get_product(product_id: int):
-    pass
+    product_with_variants = await ProductService.get_by_id(product_id)
+    return GetProductByIdResponse(product=product_with_variants)
 
 
-@router.get("/{product_id}/variants", summary="Получить все варианты конкретного продукта")
+@router.get("/{product_id}/variants", summary="Получить только варианты конкретного продукта", response_model=GetProductVariantsResponse)
 async def get_product_variants(product_id: int):
-    pass
+    variants = await ProductService.get_variants_by_product_id(product_id)
+    return GetProductVariantsResponse(variants=variants)
