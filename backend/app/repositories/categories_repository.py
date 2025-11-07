c176b6e0a87d13eb4853b41e7465142643f39140
Fiskoff <fiskoff2015@yandex.ru>
2025-11-07 13:01:26 +0700

    Created repository for products and categories

---
+++ backend/app/repositories/categories_repository.py
@@ -0,0 +1,31 @@
+import logging
+
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
+from backend.core.models import ProductCategory
+from backend.core.db_helper import db_helper
+
+
+logger = logging.getLogger(__name__)
+
+
+class ProductCategoryRepository:
+    @staticmethod
+    async def get_all() -> list[ProductCategory]:
+        async with db_helper.session_factory() as session:
+            result = await session.execute(select(ProductCategory))
+            categories = result.unique().scalars().all()
+            return categories
+
+
+    @staticmethod
+    async def get_by_id(category_id) -> ProductCategory | None:
+        async with db_helper.session_factory() as session:
+            result = await session.execute (
+                select(ProductCategory)
+                .options(selectinload(ProductCategory.products))
+                .where(ProductCategory.id == category_id)
+            )
+            category_with_products = result.scalar_one_or_none()
+            return category_with_products
--- backend/app/repositories/products_repository.py
+++ backend/app/repositories/products_repository.py
@@ -1,4 +1,42 @@
 import logging
 
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
+from backend.core.models import Product, ProductVariant
+from backend.core.db_helper import db_helper
+
 
 logger = logging.getLogger(__name__)
+
+
+class ProductRepository:
+    @staticmethod
+    async def get_all() -> list[Product]:
+        async with db_helper.session_factory() as session:
+            result = await session.execute(select(Product))
+            products = result.scalars().all()
+            return products
+
+
+    @staticmethod
+    async def get_by_id(product_id) -> Product | None:
+        async with db_helper.session_factory() as session:
+            result = await session.execute (
+                select(Product)
+                .options(selectinload(Product.variants))
+                .where(Product.id == product_id)
+            )
+            product_with_variants = result.scalar_one_or_none()
+            return product_with_variants
+
+
+    @staticmethod
+    async def get_variants_by_product_id(product_id: int) -> list[ProductVariant]:
+        async with db_helper.session_factory() as session:
+            result = await session.execute (
+                select(ProductVariant)
+                .where(ProductVariant.product_id == product_id)
+            )
+            variants = result.scalars().all()
+            return variants
