5b46ac1479d73dcadb8f9161dc654e56c16dcfa7
Fiskoff <fiskoff2015@yandex.ru>
2025-11-10 12:46:06 +0700

    Added loger and processing exception

--- backend/app/api/routers/products_routers.py
+++ backend/app/api/routers/products_routers.py
@@ -1,6 +1,7 @@
 import logging
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from starlette import status
 
 from app.services import ProductCategoryService, ProductService
 from app.api.schemas import GetCategoriesResponse, GetCategoryByIdResponse, GetProductsResponse, GetProductByIdResponse, GetProductVariantsResponse
@@ -11,31 +12,86 @@ logger = logging.getLogger(__name__)
 router = APIRouter(prefix="/products", tags=["Products"])
 
 
-@router.get("/categories", summary="Получить все категории товаров", response_model=GetCategoriesResponse)
-async def get_categories():
-    categories = await ProductCategoryService.get_all()
-    return GetCategoriesResponse(categories=categories)
+@router.get("/categories", summary="Получить все категории товаров")
+async def get_categories() -> GetCategoriesResponse:
+    logger.info(f"GET /products/categories")
+    try:
+        categories = await ProductCategoryService.get_all()
+        logger.info(f"GET /products/categories - status_code : 200")
+        return GetCategoriesResponse(categories=categories)
+    except Exception as error:
+        logger.error(f"GET /products/categories - status_code : 500")
+        logger.error(f"Error : {str(error)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Внутренняя ошибка сервера при получении категории"
+        )
 
 
-@router.get("/categories/{category_id}", summary="Получить конкретную категорию товара и её продукты", response_model=GetCategoryByIdResponse)
-async def get_category(category_id: int):
-    category_with_products = await ProductCategoryService.get_by_id(category_id)
-    return GetCategoryByIdResponse(category=category_with_products)
+@router.get("/categories/{category_id}", summary="Получить конкретную категорию товара и её продукты")
+async def get_category(category_id: int) -> GetCategoryByIdResponse:
+    logger.info(f"GET /products/categories/{category_id}")
+    try:
+        category = await ProductCategoryService.get_by_id(category_id)
+        logger.info(f"GET /products/categories/{category_id} - status_code: 200")
+        return GetCategoryByIdResponse(category=category)
+    except HTTPException:
+        raise
+    except Exception as error:
+        logger.error(f"GET /products/categories - status_code: 500, error: {str(error)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Внутренняя ошибка сервера при получении категории"
+        )
 
 
-@router.get("", summary="Получить все продукты", response_model=GetProductsResponse)
-async def get_products():
-    products = await ProductService.get_all()
-    return GetProductsResponse(products=products)
+@router.get("", summary="Получить все продукты")
+async def get_products() -> GetProductsResponse:
+    logger.info(f"GET /products")
+    try:
+        products = await ProductService.get_all()
+        logger.info(f"GET /products - status_code : 200")
+        return GetProductsResponse(products=products)
+    except Exception as error:
+        logger.error(f"GET /products - status_code : 500")
+        logger.error(f"Error : {str(error)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Внутренняя ошибка сервера при получении товаров"
+        )
 
 
-@router.get("/{product_id}", summary="Получить конкретный продукт и его варианты", response_model=GetProductByIdResponse)
-async def get_product(product_id: int):
-    product_with_variants = await ProductService.get_by_id(product_id)
-    return GetProductByIdResponse(product=product_with_variants)
+@router.get("/{product_id}", summary="Получить конкретный продукт и его варианты")
+async def get_product(product_id: int) -> GetProductByIdResponse:
+    logger.info(f"GET /{product_id}")
+    try:
+        product_with_variants = await ProductService.get_by_id(product_id)
+        logger.info(f"GET /{product_id} - status_code : 200")
+        return GetProductByIdResponse(product=product_with_variants)
+    except HTTPException:
+        raise
+    except Exception as error:
+        logger.error(f"GET /{product_id} - status_code : 500")
+        logger.error(f"Error : {str(error)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Внутренняя ошибка сервера при получении товара"
+        )
 
 
-@router.get("/{product_id}/variants", summary="Получить только варианты конкретного продукта", response_model=GetProductVariantsResponse)
-async def get_product_variants(product_id: int):
-    variants = await ProductService.get_variants_by_product_id(product_id)
-    return GetProductVariantsResponse(variants=variants)
+@router.get("/{product_id}/variants", summary="Получить только варианты конкретного продукта")
+async def get_product_variants(product_id: int) -> GetProductVariantsResponse:
+    logger.info(f"GET /{product_id}/variants")
+    try:
+        variants = await ProductService.get_variants_by_product_id(product_id)
+        logger.info(f"GET /{product_id}/variants - status_code : 200")
+        return GetProductVariantsResponse(variants=variants)
+    except HTTPException:
+        raise
+    except Exception as error:
+        logger.error(f"GET /{product_id}/variants - status_code : 500")
+        logger.error(f"Error : {str(error)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Внутренняя ошибка сервера при получении вариаций по товару"
+        )
