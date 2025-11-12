0df0b37529043a3a5f554db0e040ce783fb9b801
Fiskoff <fiskoff2015@yandex.ru>
2025-11-12 16:23:55 +0700

    Fix. Added chek is_active and valid_to

--- backend/app/repositories/promo_code_repository.py
+++ backend/app/repositories/promo_code_repository.py
@@ -1,4 +1,5 @@
 import decimal
+import datetime
 
 from sqlalchemy import select
 
@@ -9,7 +10,12 @@ from core.models import PromoCode
 class PromoCodeRepository:
     @staticmethod
     async def get_discount_by_promocode(promo_code: str) -> decimal.Decimal | None:
-        query = select(PromoCode.discount).where(PromoCode.promo_code == promo_code)
+        current_date_and_time = datetime.datetime.now()
+        query = select(PromoCode.discount).where(
+            PromoCode.promo_code == promo_code,
+            PromoCode.is_active == True,
+            PromoCode.valid_to >= current_date_and_time
+        )
         async with db_helper.session_factory() as session:
             result = await session.execute(query)
             discount = result.scalar_one_or_none()
